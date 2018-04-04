b47c63dd28d96952a0646a0631403e96f3a9afdd
Dan Kilty <dan.kilty@canada.ca>
2018-04-04 10:43:10 -0400

    Added missing package, removed tinymce demo

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,11 +1,11 @@
-import { BrowserModule } from '@angular/platform-browser';
-import { NgModule } from '@angular/core';
-import {FormsModule,ReactiveFormsModule} from '@angular/forms';
+import {BrowserModule} from '@angular/platform-browser';
+import {NgModule} from '@angular/core';
+import {FormsModule, ReactiveFormsModule} from '@angular/forms';
 
-import { AppComponent } from './app.component';
+import {AppComponent} from './app.component';
 import {ValidationService} from './validation.service';
 import {ErrorModule} from './error-msg/error-ui.module';
-import { CompanyBaseComponent } from './company-base/company-base.component';
+import {CompanyBaseComponent} from './company-base/company-base.component';
 import {MainPipeModule} from './main-pipe/main-pipe.module';
 import {GlobalsService} from './globals/globals.service';
 import {AddressModule} from './address/address.module';
@@ -17,8 +17,6 @@ import {HttpClient, HttpClientModule} from '@angular/common/http';
 import {CommonFeatureModule} from './common/common-feature.module';
 import {TherapeuticModule} from './therapeutic/therapeutic.module';
 import {NgbModule} from '@ng-bootstrap/ng-bootstrap';
-import { TinyMceModule } from 'angular-tinymce';
-import { tinymceDefaultSettings } from 'angular-tinymce';
 import {DataLoaderModule} from './data-loader/data-loader.module';
 import {CompanyDataLoaderService} from './data-loader/company-data-loader.service';
 
@@ -46,23 +44,23 @@ import {CompanyDataLoaderService} from './data-loader/company-data-loader.servic
         deps: [HttpClient]
       }
     }),
-    NgbModule.forRoot(),
-    TinyMceModule.forRoot(tinymceDefaultSettings())
+    NgbModule.forRoot()
   ],
   providers: [
     ValidationService,
     GlobalsService,
     CompanyDataLoaderService
   ],
-  exports:[
+  exports: [
     TranslateModule
   ],
   bootstrap: [AppComponent]
 })
-export class AppModule { }
+export class AppModule {
+}
 
 
 export function HttpLoaderFactory(http: HttpClient) {
-  //return new TranslateHttpLoader(http);
+  // return new TranslateHttpLoader(http);
   return new TranslateHttpLoader(http, './assets/i18n/', '.json');
 }
