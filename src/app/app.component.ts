30f4923c32445bbedec31a062c081241f46b9208
Dan Kilty <dan.kilty@canada.ca>
2018-04-26 12:07:10 -0400

    Env test for lang

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,5 +1,6 @@
 import {Component} from '@angular/core';
 import {TranslateService} from '@ngx-translate/core';
+import { environment } from '../environments/environment';
 /*declare var xml2js: any;*/
 
 // TODO this needs to not be in the nase component. base should have almost nothing in it, generic
@@ -15,10 +16,11 @@ export class AppComponent {
 
 // we will use form builder to simplify our syntax
   constructor(private translate: TranslateService) {
-    translate.setDefaultLang('fr');
+    translate.setDefaultLang(environment.lang);
+    console.log(environment.lang)
     this.translateInstance = translate;
     // the lang to use, if the lang isn't available, it will use the current loader to get them
-    translate.use('en');
+    translate.use(environment.lang);
     this.translate.get('commmon.required.bracket').subscribe(res => {
       console.log(res);
     });
