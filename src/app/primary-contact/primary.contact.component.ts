d9e6674b76144071c8a1a07e74d376f4e0ee1fc4
stevezhaoca <steve.zhao@canada.ca>
2019-04-18 17:14:08 -0400

    in developing - co form

---
+++ src/app/primary-contact/primary.contact.component.ts
@@ -0,0 +1,127 @@
+import {
+  Component, Input, Output, OnInit, SimpleChanges, OnChanges, EventEmitter, ViewChildren, QueryList,
+  AfterViewInit, ChangeDetectionStrategy, ChangeDetectorRef
+} from '@angular/core';
+import {FormGroup, FormBuilder} from '@angular/forms';
+import {ControlMessagesComponent} from '../error-msg/control-messages.component/control-messages.component';
+import {PrimaryContactService} from './primary.contact.service';
+import {isArray} from 'util';
+import {noUndefined} from '@angular/compiler/src/util';
+
+@Component({
+  selector: 'primary-contact',
+  templateUrl: 'primary.contact.component.html'
+})
+
+/**
+ * Primary Contact component is used for company form
+ */
+export class PrimaryContactComponent implements OnInit, OnChanges, AfterViewInit {
+
+  public primContactFormLocalModel: FormGroup;
+  @Input('group') public primContactFormRecord: FormGroup;
+  @Input() detailsChanged: number;
+  @Input() showErrors: boolean;
+  @Input() primContactModel;
+  @Input() lang;
+  @Output() primContactErrorList = new EventEmitter(true);
+  @ViewChildren(ControlMessagesComponent) msgList: QueryList<ControlMessagesComponent>;
+
+  public showFieldErrors = false;
+  public yesNoList: Array<any> = [];
+  private detailsService: PrimaryContactService;
+
+  constructor(private _fb: FormBuilder, private cdr: ChangeDetectorRef) {
+    this.showFieldErrors = false;
+    this.showErrors = false;
+    this.detailsService = new PrimaryContactService();
+    this.yesNoList = this.detailsService.getYesNoList();
+  }
+
+  ngOnInit() {
+    if (!this.primContactFormLocalModel) {
+      this.primContactFormLocalModel = PrimaryContactService.getReactiveModel(this._fb);
+    }
+    this.detailsChanged = 0;
+  }
+
+  ngAfterViewInit() {
+    this.msgList.changes.subscribe(errorObjs => {
+      let temp = [];
+      this._updateErrorList(errorObjs);
+    });
+    this.msgList.notifyOnChanges();
+
+  }
+
+  private _updateErrorList(errorObjs) {
+    let temp = [];
+    if (errorObjs) {
+      errorObjs.forEach(
+        error => {
+          temp.push(error);
+        }
+      );
+    }
+    this.primContactErrorList.emit(temp);
+
+  }
+
+  ngDoCheck() {
+    /*  this.isValid();
+      this._syncCurrentExpandedRow();*/
+  }
+
+  ngOnChanges(changes: SimpleChanges) {
+
+    // since we can't detect changes on objects, using a separate flag
+    if (changes['detailsChanged']) { // used as a change indicator for the model
+      // console.log("the details cbange");
+      if (this.primContactFormRecord) {
+        this.setToLocalModel();
+
+      } else {
+        this.primContactFormLocalModel = PrimaryContactService.getReactiveModel(this._fb);
+        this.primContactFormLocalModel.markAsPristine();
+      }
+    }
+    if (changes['showErrors']) {
+
+      this.showFieldErrors = changes['showErrors'].currentValue;
+      let temp = [];
+      if (this.msgList) {
+        this.msgList.forEach(item => {
+          temp.push(item);
+          // console.log(item);
+        });
+      }
+      this.primContactErrorList.emit(temp);
+    }
+    if (changes['primContactFormLocalModel']) {
+      console.log('**********the primary contact changed');
+      this.primContactFormRecord = this.primContactFormLocalModel;
+    }
+    if (changes['primContactModel']) {
+      const dataModel = changes['primContactModel'].currentValue;
+      PrimaryContactService.mapDataModelToFormModel(dataModel, (<FormGroup>this.primContactFormLocalModel));
+    }
+  }
+
+  /**
+   * Uses the updated reactive forms model locally
+   */
+
+  setToLocalModel() {
+    this.primContactFormLocalModel = this.primContactFormRecord;
+    if (!this.primContactFormLocalModel.pristine) {
+      this.primContactFormLocalModel.markAsPristine();
+    }
+  }
+
+  onblur() {
+    // console.log('input is typed');
+    PrimaryContactService.mapFormModelToDataModel((<FormGroup>this.primContactFormLocalModel),
+      this.primContactModel);
+  }
+}
+
---
+++ src/app/primary-contact/primary.contact.service.ts
@@ -0,0 +1,98 @@
+import {AfterViewInit, Injectable, OnChanges, OnInit} from '@angular/core';
+import {FormBuilder, FormGroup, Validators} from '@angular/forms';
+import {GlobalsService} from '../globals/globals.service';
+import {ValidationService} from '../validation.service';
+
+@Injectable()
+export class PrimaryContactService {
+
+  constructor() {
+  }
+
+  /**
+   * Gets an yesno array
+   *
+   */
+  public getYesNoList() {
+    return [
+      GlobalsService.YES,
+      GlobalsService.NO
+    ];
+  }
+
+  /**
+   * Gets the reactive forms Model for address details
+   * @param {FormBuilder} fb
+   * @returns {any}
+   */
+  public static getReactiveModel(fb: FormBuilder) {
+    if (!fb) {
+      return null;
+    }
+    return fb.group({
+      isThirdParty: '',
+      repContactCompanyId: [null, [Validators.required, ValidationService.primaryCompanyIdValidator]],
+      repContactId: [null, [Validators.required, ValidationService.primaryContactIdValidator]],
+      repContactName: [null, Validators.required],
+    });
+  }
+
+  /**
+   * Gets an empty model
+   *
+   */
+  public static getEmptyModel() {
+
+    return (
+      {
+        is_third_party: '',
+        rep_contact_company_id: '',
+        rep_contact_id: '',
+        rep_contact_name: ''
+      }
+    );
+  }
+
+  public static mapFormModelToDataModel(formRecord: FormGroup, primContactModel) {
+    primContactModel.is_third_party = formRecord.controls.isThirdParty.value ? GlobalsService.YES : GlobalsService.NO;
+    primContactModel.rep_contact_company_id = formRecord.controls.repContactCompanyId.value;
+    primContactModel.rep_contact_id = formRecord.controls.repContactId.value;
+    primContactModel.rep_contact_name = formRecord.controls.repContactName.value;
+  }
+
+  public static mapDataModelToFormModel(primContactModel, formRecord: FormGroup) {
+    const itp = primContactModel.is_third_party === GlobalsService.YES ? true : false;
+    formRecord.controls.isThirdParty.setValue(itp);
+    formRecord.controls.repContactCompanyId.setValue(primContactModel.rep_contact_company_id);
+    formRecord.controls.repContactId.setValue(primContactModel.rep_contact_id);
+    formRecord.controls.repContactName.setValue(primContactModel.rep_contact_name);
+  }
+
+  public static getRecordId(record: FormGroup) {
+    return (record.controls.id.value);
+  }
+
+  public static setRecordId(record: FormGroup, value: number): void {
+    if (!record) {
+      return;
+    }
+    record.controls.id.setValue(value);
+  }
+
+  /**
+   * Find a record by its unique id,. If a dup, returns first instance
+   * @param list
+   * @param criteria
+   * @returns {any}
+   */
+  public static findRecordByTerm(list, criteria, searchTerm) {
+
+    let result = list.filter(
+      item => item[searchTerm] === criteria[searchTerm]);
+    if (result && result.length > 0) {
+      return result[0];
+    }
+    return null;
+  }
+
+}
