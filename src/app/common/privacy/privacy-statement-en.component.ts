dcb9d3ba43b97fc3be3ba49492a52fa0958dd936
IanYangCa <IanYang.ca@gmail.com>
2020-01-21 09:57:33 -0500

    WCAG changes.

---
+++ src/app/common/privacy/privacy-statement-en.component.ts
@@ -0,0 +1,10 @@
+import {Component} from '@angular/core';
+
+@Component({
+  selector: 'app-privacy-component',
+  templateUrl: './privacy-statement-en-component.html',
+})
+
+export class AppPrivacyComponent {
+
+}
---
+++ src/app/common/security/security-disclaimer-en.component.ts
@@ -0,0 +1,10 @@
+import {Component} from '@angular/core';
+
+@Component({
+  selector: 'app-security-component',
+  templateUrl: './security-disclaimer-en-component.html'
+})
+
+export class AppSecurityComponent {
+
+}
