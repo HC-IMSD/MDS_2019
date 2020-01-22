7a18e30c1e29102e42f77ce78b11bd77df1f132c
IanYangCa <IanYang.ca@gmail.com>
2020-01-22 15:16:34 -0500

    Added privacy and security components

--- src/app/common/common-feature.module.ts
+++ src/app/common/common-feature.module.ts
@@ -3,18 +3,24 @@ import { CommonModule } from '@angular/common';
 import {ExpanderComponent} from './expander/expander.component';
 import {BrowserModule} from '@angular/platform-browser';
 import {ReactiveFormsModule} from '@angular/forms';
+import {AppPrivacyComponent} from './privacy/privacy-statement-en.component';
+import {AppSecurityComponent} from './security/security-disclaimer-en.component';
 
 @NgModule({
   imports: [
     CommonModule,
     BrowserModule,
-    ReactiveFormsModule,
+    ReactiveFormsModule
   ],
   declarations: [
-    ExpanderComponent
+    ExpanderComponent,
+    AppPrivacyComponent,
+    AppSecurityComponent
   ],
   exports: [
-    ExpanderComponent
+    ExpanderComponent,
+    AppPrivacyComponent,
+    AppSecurityComponent
   ]
 })
 export class CommonFeatureModule {
